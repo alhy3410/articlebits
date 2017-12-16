b885ea3e417b8cdbc5756665fba3d5d0c15406a8
Andrew Lee <andrewlhy@gmail.com>
2017-12-16 08:07:36 -0800

    Delete mysqlconnection.py

--- mysqlconnection.py
+++
@@ -1,9 +0,0 @@
-import pymysql.cursors
-from credentials import password,user
-
-connection = pymysql.connect(host='localhost',
-                             user=user,
-                             password=password,
-                             db='news_scanner',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
